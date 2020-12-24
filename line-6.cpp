a26ea1aec7e678cef68c66f33cdb2e705d37b97e
alenahalm <alenahalmatova749@gmail.com>
2020-12-24 14:18:29 +0800

    Initial commit

---
+++ line-6.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	double a, r = 0;
+	cout << "Enter a and radius" << endl;
+	cin >> a >> r;
+	n = ((a / 2 * r) - sqrt(3) + 1);
+	if (n <= 0) {
+		n = 0;
+	}
+	else {
+		n = n * (n + 1) / 2;
+	}
+	cout << n << endl;
+	system("pause");
+}
